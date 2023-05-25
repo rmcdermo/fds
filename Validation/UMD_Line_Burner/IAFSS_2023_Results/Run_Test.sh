5a5d62152930434298e8edaa096d79602f1c2bac
rmcdermo <randy.mcdermott@gmail.com>
2023-05-25 13:33:35 -0400

    IAFSS 2023: add run script

---
+++ Validation/UMD_Line_Burner/IAFSS_2023_Results/Run_Test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# This script runs a set of Validation Cases on a Linux machine with a batch queuing system.
+# See the file Validation/Common_Run_All.sh for more information.
+export INDIR=`pwd`/Thermally_Thick_Burner
+export QFDS=$FIREMODELS/fds/Utilities/Scripts/qfds.sh
+export EMAIL=randall.mcdermott@nist.gov
+export QUEUE=batch2
+
+$QFDS -b $EMAIL -p 14 -q $QUEUE -d $INDIR methane_dx_2p5cm.fds
+$QFDS -b $EMAIL -p 14 -q $QUEUE -d $INDIR propane_dx_2p5cm.fds
+$QFDS -b $EMAIL -p 14 -q $QUEUE -d $INDIR methane_XO2_p18_dx_2p5cm.fds
+
+$QFDS -b $EMAIL -p 24 -q $QUEUE -d $INDIR methane_dx_1p25cm.fds
+$QFDS -b $EMAIL -p 24 -q $QUEUE -d $INDIR propane_dx_1p25cm.fds
+$QFDS -b $EMAIL -p 24 -q $QUEUE -d $INDIR methane_XO2_p18_dx_1p25cm.fds
+
+$QFDS -b $EMAIL -p 164 -q $QUEUE -d $INDIR methane_dx_p625cm.fds
+$QFDS -b $EMAIL -p 164 -q $QUEUE -d $INDIR propane_dx_p625cm.fds
+$QFDS -b $EMAIL -p 164 -q $QUEUE -d $INDIR methane_XO2_p18_dx_p625cm.fds
+
+$QFDS -b $EMAIL -p 204 -q $QUEUE -d $INDIR methane_dx_p3125cm.fds
+$QFDS -b $EMAIL -p 204 -q $QUEUE -d $INDIR propane_dx_p3125cm.fds
+$QFDS -b $EMAIL -p 204 -q $QUEUE -d $INDIR methane_XO2_p18_dx_p3125cm.fds
+
+echo FDS cases submitted
