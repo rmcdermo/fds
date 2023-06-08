278f7db3a0f95d4a3a1d38146cfdd9ac65c2186c
rmcdermo <randy.mcdermott@gmail.com>
2023-06-08 15:30:05 -0400

    IAFSS 2023: add rmsu script

---
+++ Validation/UMD_Line_Burner/IAFSS_2023_Results/Scripts/umd_line_burner_rmsu.m
@@ -0,0 +1,447 @@
+% McDermott
+% 1-9-2017, updated 26 Jan 2023 for IAFSS
+% umd_line_burner_eta.m
+%
+% Plot combustion efficiency for UMD Line Burner cases.
+
+close all
+clear all
+
+Lf_pts = 50;
+Lf_min = 0;
+Lf_max = 1;
+Lf_dz  = (Lf_max-Lf_min)/(Lf_pts-1);
+Lf_z   = Lf_min:Lf_dz:Lf_max;
+Lf_fac = 0.97;
+Lf_dt  = 10;
+
+plot_style
+
+%expdir = '../../../../../exp/Submodules/macfp-db/Extinction/UMD_Line_Burner/Experimental_Data/';
+expdir = 'D:\TRI\macfp-db-master\macfp-db-master\Extinction\UMD_Line_Burner\Experimental_Data\';
+% outdir = '../Baseline/';
+outdir = 'D:\TRI\UMD\Final_version\TRI0_Final\Current_Results\';
+%'D:\TRI\UMD\Final_version\TRI0_Final\Current_Results\' %TRI0
+%D:\TRI\UMD\Final_version\TRI-3\Current_Results %TRI3
+%D:\TRI\UMD\Final_version\Baseline\Baseline %Baseline
+%D:\TRI\UMD\Final_version\TRI0_AIT_ZONE_2 %TRI_0 with AIT
+%pltdir = '../Plots/';
+pltdir = 'D:\TRI\UMD\Final_version\Scripts';
+
+exp_fname    = {'CH4_A_Data.csv','C3H8_A_Data.csv'};
+exp_Lf_fname = {'CH4_A_Lf_Data.csv','C3H8_A_Lf_Data.csv'};
+fuel_name    = {'methane','propane'};
+Fuel_name    = {'Methane','Propane'};
+fuel_hoc     = [50010.3475,46334.6246]; % from .out file
+git_tag_ext  = '_dx_1p25cm_git.txt';
+
+line_fmt = {'b--','r--','k--',};
+key_fmt  = {'{\it W/dx}=4','{\it W/dx}=8','{\it W/dx}=16'};
+
+i_fuel =1
+
+
+% experimental results
+EXP = importdata([expdir,exp_fname{i_fuel}],',',1);
+XO2   = EXP.data(:,find(strcmp(EXP.colheaders,'XO2')));
+q_R   = EXP.data(:,find(strcmp(EXP.colheaders,'q_R')));
+Chi_R = EXP.data(:,find(strcmp(EXP.colheaders,'Chi_R')));
+S_XO2 = EXP.data(:,find(strcmp(EXP.colheaders,'S_XO2'))); % uncertainty
+eta   = EXP.data(:,find(strcmp(EXP.colheaders,'eta')));
+S_eta = EXP.data(:,find(strcmp(EXP.colheaders,'S_eta')));
+
+EXP_Lf = importdata([expdir,exp_Lf_fname{i_fuel}],',',1);
+XO2_Lf = EXP_Lf.data(:,find(strcmp(EXP_Lf.colheaders,'XO2_Lf')));
+Lf     = EXP_Lf.data(:,find(strcmp(EXP_Lf.colheaders,'Lf')));
+S_Lf   = EXP_Lf.data(:,find(strcmp(EXP_Lf.colheaders,'S_Lf')));
+
+is_case_1_run=1; if ~exist([outdir,fuel_name{i_fuel},'_dx_1p25cm_hrr.csv']); is_case_1_run=0; end
+is_case_2_run=1; if ~exist([outdir,fuel_name{i_fuel},'_dx_p625cm_hrr.csv']); is_case_2_run=0; end
+is_case_3_run=1; if ~exist([outdir,fuel_name{i_fuel},'_dx_p3125cm_hrr.csv']); is_case_3_run=0; end
+
+row_start = 5;
+
+if is_case_1_run % case 1 if
+    % dx=1.25 cm results
+    HRR1 = importdata([outdir,fuel_name{i_fuel},'_dx_1p25cm_hrr.csv'],',',2);
+    DEV1 = importdata([outdir,fuel_name{i_fuel},'_dx_1p25cm_devc.csv'],',',2);
+
+    Time_FDS_1 = DEV1.data(row_start:end,find(strcmp(DEV1.colheaders,'Time')));
+    XO2_FDS_1 = DEV1.data(row_start:end,find(strcmp(DEV1.colheaders,'"XO2"')));
+    q_R_FDS_1 = 0.5*( DEV1.data(row_start:end,find(strcmp(DEV1.colheaders,'"qrad1"'))) + DEV1.data(row_start:end,find(strcmp(DEV1.colheaders,'"qrad2"'))) );
+    HRR_FDS_1 = HRR1.data(row_start:end,find(strcmp(HRR1.colheaders,'HRR')));
+    switch i_fuel
+        case 1
+            MLR_FDS_1 = HRR1.data(row_start:end,find(strcmp(HRR1.colheaders,'MLR_METHANE')));
+        case 2
+            MLR_FDS_1 = HRR1.data(row_start:end,find(strcmp(HRR1.colheaders,'MLR_PROPANE')));
+    end
+    eta_FDS_1 = HRR_FDS_1./(MLR_FDS_1*fuel_hoc(i_fuel));
+    QR_FDS_1 = abs(HRR1.data(row_start:end,find(strcmp(HRR1.colheaders,'Q_RADI'))));
+    GLOB_CHI_R_1 = QR_FDS_1./(MLR_FDS_1*fuel_hoc(i_fuel)); % Q in denom based on definition of X_rad in James White thesis
+    L_F_1 = DEV1.data(row_start:end,find(strcmp(DEV1.colheaders,'"L_F"')));
+
+    % % flame height
+    % colLf01 = find(strcmp(DEV1.colheaders,'"Lf-1"'));
+    % colLf50 = find(strcmp(DEV1.colheaders,'"Lf-50"'));
+    % Lf_FDS_1 = zeros(1,length(Time_FDS_1));
+    % for n=1:length(Time_FDS_1)
+    %     hrrpul = DEV1.data(n,colLf01:colLf50);
+    %     q_total = sum(hrrpul);
+    %     q_sum = 0.;
+    %     for i=1:Lf_pts
+    %         q_sum = q_sum+hrrpul(i);
+    %         if q_sum>Lf_fac*q_total
+    %             Lf_FDS_1(n) = Lf_z(i);
+    %             break;
+    %         end
+    %     end
+    % end
+    % Lf_tmp = Lf_FDS_1;
+    % for n=1:length(Time_FDS_1)
+    %     indx_range = [find(Time_FDS_1>(Time_FDS_1(n)-Lf_dt),1):n];
+    %     Lf_FDS_1(n) = mean(Lf_tmp(indx_range));
+    % end
+end % case 1 if
+
+if is_case_2_run % case 2 if
+    % dx=0.625 cm results
+    HRR2 = importdata([outdir,fuel_name{i_fuel},'_dx_p625cm_hrr.csv'],',',2);
+    DEV2 = importdata([outdir,fuel_name{i_fuel},'_dx_p625cm_devc.csv'],',',2);
+
+    Time_FDS_2 = DEV2.data(row_start:end,find(strcmp(DEV2.colheaders,'Time')));
+    XO2_FDS_2 = DEV2.data(row_start:end,find(strcmp(DEV2.colheaders,'"XO2"')));
+    q_R_FDS_2 = 0.5*( DEV2.data(row_start:end,find(strcmp(DEV2.colheaders,'"qrad1"'))) + DEV2.data(row_start:end,find(strcmp(DEV2.colheaders,'"qrad2"'))) );
+    HRR_FDS_2 = HRR2.data(row_start:end,find(strcmp(HRR2.colheaders,'HRR')));
+    switch i_fuel
+        case 1
+            MLR_FDS_2 = HRR2.data(row_start:end,find(strcmp(HRR2.colheaders,'MLR_METHANE')));
+        case 2
+            MLR_FDS_2 = HRR2.data(row_start:end,find(strcmp(HRR2.colheaders,'MLR_PROPANE')));
+    end
+    eta_FDS_2 = HRR_FDS_2./(MLR_FDS_2*fuel_hoc(i_fuel));
+    QR_FDS_2 = abs(HRR2.data(row_start:end,find(strcmp(HRR2.colheaders,'Q_RADI'))));
+    GLOB_CHI_R_2 = QR_FDS_2./(MLR_FDS_2*fuel_hoc(i_fuel)); % Q in denom based on definition of X_rad in James White thesis
+    L_F_2 = DEV2.data(row_start:end,find(strcmp(DEV2.colheaders,'"L_F"')));
+
+    % % flame height
+    % colLf01 = find(strcmp(DEV2.colheaders,'"Lf-1"'));
+    % colLf50 = find(strcmp(DEV2.colheaders,'"Lf-50"'));
+    % Lf_FDS_2 = zeros(1,length(Time_FDS_2));
+    % for n=1:length(Time_FDS_2)
+    %     hrrpul = DEV2.data(n,colLf01:colLf50);
+    %     q_total = sum(hrrpul);
+    %     q_sum = 0.;
+    %     for i=1:Lf_pts
+    %         q_sum = q_sum+hrrpul(i);
+    %         if q_sum>Lf_fac*q_total
+    %             Lf_FDS_2(n) = Lf_z(i);
+    %             break;
+    %         end
+    %     end
+    % end
+    % Lf_tmp = Lf_FDS_2;
+    % for n=1:length(Time_FDS_2)
+    %     indx_range = [find(Time_FDS_2>(Time_FDS_2(n)-Lf_dt),1):n];
+    %     Lf_FDS_2(n) = mean(Lf_tmp(indx_range));
+    % end
+end % case 2 if
+
+if is_case_3_run % case 3 if
+    % dx=0.3125 cm results
+    HRR3 = importdata([outdir,fuel_name{i_fuel},'_dx_p3125cm_hrr.csv'],',',2);
+    DEV3 = importdata([outdir,fuel_name{i_fuel},'_dx_p3125cm_devc.csv'],',',2);
+
+    Time_FDS_3 = DEV3.data(row_start:end,find(strcmp(DEV3.colheaders,'Time')));
+    XO2_FDS_3 = DEV3.data(row_start:end,find(strcmp(DEV3.colheaders,'"XO2"')));
+    q_R_FDS_3 = 0.5*( DEV3.data(row_start:end,find(strcmp(DEV3.colheaders,'"qrad1"'))) + DEV3.data(row_start:end,find(strcmp(DEV3.colheaders,'"qrad2"'))) );
+    HRR_FDS_3 = HRR3.data(row_start:end,find(strcmp(HRR3.colheaders,'HRR')));
+    switch i_fuel
+        case 1
+            MLR_FDS_3 = HRR3.data(row_start:end,find(strcmp(HRR3.colheaders,'MLR_METHANE')));
+        case 2
+            MLR_FDS_3 = HRR3.data(row_start:end,find(strcmp(HRR3.colheaders,'MLR_PROPANE')));
+    end
+    eta_FDS_3 = HRR_FDS_3./(MLR_FDS_3*fuel_hoc(i_fuel));
+    QR_FDS_3 = abs(HRR3.data(row_start:end,find(strcmp(HRR3.colheaders,'Q_RADI'))));
+    GLOB_CHI_R_3 = QR_FDS_3./(MLR_FDS_3*fuel_hoc(i_fuel)); % Q in denom based on definition of X_rad in James White thesis
+    L_F_3 = DEV3.data(row_start:end,find(strcmp(DEV3.colheaders,'"L_F"')));
+%X_O2
+    % % flame height
+    % colLf01 = find(strcmp(DEV3.colheaders,'"Lf-1"'));
+    % colLf50 = find(strcmp(DEV3.colheaders,'"Lf-50"'));
+    % Lf_FDS_3 = zeros(1,length(Time_FDS_3));
+    % for n=1:length(Time_FDS_3)
+    %     hrrpul = DEV3.data(n,colLf01:colLf50);
+    %     q_total = sum(hrrpul);
+    %     q_sum = 0.;
+    %     for i=1:Lf_pts
+    %         q_sum = q_sum+hrrpul(i);
+    %         if q_sum>Lf_fac*q_total
+    %             Lf_FDS_3(n) = Lf_z(i);
+    %             break;
+    %         end
+    %     end
+    % end
+    % Lf_tmp = Lf_FDS_3;
+    % for n=1:length(Time_FDS_3)
+    %     indx_range = [find(Time_FDS_3>(Time_FDS_3(n)-Lf_dt),1):n];
+    %     Lf_FDS_3(n) = mean(Lf_tmp(indx_range));
+    % end
+end % case 3 if
+
+
+% compute CHI_R ramp
+figure
+set(gca,'Units',Plot_Units)
+set(gca,'Position',[Plot_X Plot_Y Plot_Width Plot_Height])
+
+X_O2_max = 2.077972E-01;
+X_O2_min = 0.1;
+ramp_time = @(x) (x-X_O2_max)*60./(X_O2_min-X_O2_max); % this comes from linear XO2 ramp (see input file)
+H(1)=plot(XO2,Chi_R); hold on
+switch i_fuel
+    case 1 % methane Chi_r ramp
+        pw_ramp_time = [0,37,45,60];
+        pw_ramp_XO2  = [0.21,0.142,0.123,0];
+        pw_ramp_chir = [.245,.125,0,0];
+    case 2
+        pw_ramp_time = [0,37,45,60];
+        pw_ramp_XO2  = [0.21,.17,0.133,0.129,0];
+        pw_ramp_chir = [.33,.245,.125,0,0];
+end
+H(2)=plot(pw_ramp_XO2,pw_ramp_chir);
+
+if is_case_1_run | is_case_2_run | is_case_3_run
+    is_case_run = 1;
+else
+    is_case_run = 0;
+end
+
+clear H
+figure
+set(gca,'Units',Plot_Units)
+set(gca,'Position',[Plot_X Plot_Y Plot_Width Plot_Height])
+
+steel_blue = [0 0.447 0.741];
+H(1)=plot(XO2,eta,'.','MarkerSize',10); hold on
+set(H(1),'Color',steel_blue)
+subr= 1:10:length(XO2);
+h=errorbar(XO2(subr),eta(subr),-S_eta(subr),S_eta(subr),-S_XO2(subr),S_XO2(subr),'.','MarkerSize',10); hold on
+set(h,'Color',steel_blue)
+if is_case_run
+    if is_case_1_run; H(2)=plot(XO2_FDS_1,eta_FDS_1,line_fmt{1},'LineWidth',2); end
+    if is_case_2_run; H(3)=plot(XO2_FDS_2,eta_FDS_2,line_fmt{2},'LineWidth',2); end
+    if is_case_3_run; H(4)=plot(XO2_FDS_3,eta_FDS_3,line_fmt{3},'LineWidth',2); end
+    axis([0.09 0.21 0 1.2 ])
+    xlabel('O2 (vol frac)','Interpreter',Font_Interpreter,'FontSize',Label_Font_Size)
+    ylabel('\eta','Interpreter',Font_Interpreter,'FontSize',Label_Font_Size)
+    text(0.095,1.12,[Fuel_name{i_fuel},' Combustion Efficiency'],'FontName',Font_Name,'FontSize',Title_Font_Size)
+
+    set(gca,'FontName',Font_Name)
+    set(gca,'FontSize',Label_Font_Size)
+
+    lh=legend(H,'Exp',key_fmt{:},'Location','SouthEast');
+    set(lh,'FontName',Font_Name,'FontSize',Key_Font_Size)
+
+    git_file=[outdir,fuel_name{i_fuel},git_tag_ext];
+    addverstr(gca,git_file,'linear');
+
+    set(gcf,'Visible',Figure_Visibility);
+    set(gcf,'Units',Paper_Units);
+    set(gcf,'PaperUnits',Paper_Units);
+    set(gcf,'PaperSize',[Paper_Width Paper_Height]);
+    set(gcf,'Position',[0 0 Paper_Width Paper_Height]);
+    print(gcf,'-dpdf',[pltdir,fuel_name{i_fuel},'_eta']);
+end
+
+% double check N2 ramp
+
+% intended ramp:
+Time_ramp = [0,60];
+XO2_ramp = [X_O2_max,X_O2_min];
+figure
+clear H
+set(gca,'Units',Plot_Units)
+set(gca,'Position',[Plot_X Plot_Y Plot_Width Plot_Height])
+H(1)=plot(Time_ramp,XO2_ramp,'--o','MarkerSize',10); hold on
+if is_case_run
+    if is_case_1_run; H(2)=plot(Time_FDS_1,XO2_FDS_1,line_fmt{1}); end
+    if is_case_2_run; H(3)=plot(Time_FDS_2,XO2_FDS_2,line_fmt{2}); end
+    if is_case_3_run; H(4)=plot(Time_FDS_3,XO2_FDS_3,line_fmt{3}); end
+    axis([0 60 0.1 0.21])
+    xlabel('Time (s)','Interpreter',Font_Interpreter,'FontSize',Label_Font_Size)
+    ylabel('O2 (vol frac)','Interpreter',Font_Interpreter,'FontSize',Label_Font_Size)
+    set(gca,'FontName',Font_Name)
+    set(gca,'FontSize',Label_Font_Size)
+    lh=legend(H,'Exp',key_fmt{:},'Location','SouthWest');
+    set(lh,'FontName',Font_Name,'FontSize',Key_Font_Size)
+
+    git_file=[outdir,fuel_name{i_fuel},git_tag_ext];
+    addverstr(gca,git_file,'linear');
+
+    set(gcf,'Visible',Figure_Visibility);
+    set(gcf,'Units',Paper_Units);
+    set(gcf,'PaperUnits',Paper_Units);
+    set(gcf,'PaperSize',[Paper_Width Paper_Height]);
+    set(gcf,'Position',[0 0 Paper_Width Paper_Height]);
+    print(gcf,'-dpdf',[pltdir,fuel_name{i_fuel},'_N2_ramp_check']);
+end
+
+% plot heat flux
+figure
+clear H
+set(gca,'Units',Plot_Units)
+set(gca,'Position',[Plot_X Plot_Y Plot_Width Plot_Height])
+H(1)=plot(XO2,q_R,'o'); hold on
+if is_case_run
+    if is_case_1_run; H(2)=plot(XO2_FDS_1,q_R_FDS_1,line_fmt{1}); end
+    if is_case_2_run; H(3)=plot(XO2_FDS_2,q_R_FDS_2,line_fmt{2}); end
+    if is_case_3_run; H(4)=plot(XO2_FDS_3,q_R_FDS_3,line_fmt{3}); end
+    axis([0.10 0.21 0 1.5])
+    xlabel('O2 (vol frac)','Interpreter',Font_Interpreter,'FontSize',Label_Font_Size)
+    ylabel('{\itq"}_R (kW/m^2)','Interpreter',Font_Interpreter,'FontSize',Label_Font_Size)
+    set(gca,'FontName',Font_Name)
+    set(gca,'FontSize',Label_Font_Size)
+    lh=legend(H,'Exp',key_fmt{:},'Location','SouthEast');
+    set(lh,'FontName',Font_Name,'FontSize',Key_Font_Size)
+
+    git_file=[outdir,fuel_name{i_fuel},git_tag_ext];
+    addverstr(gca,git_file,'linear');
+
+    set(gcf,'Visible',Figure_Visibility);
+    set(gcf,'Units',Paper_Units);
+    set(gcf,'PaperUnits',Paper_Units);
+    set(gcf,'PaperSize',[Paper_Width Paper_Height]);
+    set(gcf,'Position',[0 0 Paper_Width Paper_Height]);
+    print(gcf,'-dpdf',[pltdir,fuel_name{i_fuel},'_rad_heat_flux']);
+end
+
+% % plot flame height
+% figure
+% clear H
+% set(gca,'Units',Plot_Units)
+% set(gca,'Position',[Plot_X Plot_Y Plot_Width Plot_Height])
+% H(1)=plot(XO2_Lf,Lf,'o'); hold on
+% subr=1:2:length(Lf);
+% h=errorbar(XO2_Lf(subr),Lf(subr),-S_Lf(subr),S_Lf(subr),'o'); hold on
+% set(h,'Color',steel_blue)
+% if is_case_run
+%     if is_case_1_run; H(2)=plot(XO2_FDS_1,L_F_1,line_fmt{1}); end
+%     if is_case_2_run; H(3)=plot(XO2_FDS_2,L_F_2,line_fmt{2}); end
+%     if is_case_3_run; H(4)=plot(XO2_FDS_3,L_F_3,line_fmt{3}); end
+%     axis([0.14 0.22 0.4 1.00])
+%     xlabel('O2 (vol frac)','Interpreter',Font_Interpreter,'FontSize',Label_Font_Size)
+%     ylabel('{\itL}_f (m)','Interpreter',Font_Interpreter,'FontSize',Label_Font_Size)
+%     set(gca,'FontName',Font_Name)
+%     set(gca,'FontSize',Label_Font_Size)
+%     lh=legend(H,'Exp',key_fmt{:},'Location','NorthEast');
+%     set(lh,'FontName',Font_Name,'FontSize',Key_Font_Size)
+
+%     git_file=[outdir,fuel_name{i_fuel},git_tag_ext];
+%     addverstr(gca,git_file,'linear');
+
+%     set(gcf,'Visible',Figure_Visibility);
+%     set(gcf,'Units',Paper_Units);
+%     set(gcf,'PaperUnits',Paper_Units);
+%     set(gcf,'PaperSize',[Paper_Width Paper_Height]);
+%     set(gcf,'Position',[0 0 Paper_Width Paper_Height]);
+%     print(gcf,'-dpdf',[pltdir,fuel_name{i_fuel},'_flame_height']);
+% end
+
+% plot actual global radiative fraction
+figure
+clear H
+set(gca,'Units',Plot_Units)
+set(gca,'Position',[Plot_X Plot_Y Plot_Width Plot_Height])
+H(1)=plot(XO2,Chi_R,'ko','MarkerSize',5,'LineWidth',1.25); hold on
+set(H(1),'Color','Black')
+if is_case_run
+    if is_case_1_run; H(2)=plot(XO2_FDS_1,GLOB_CHI_R_1,line_fmt{1},'LineWidth',2); end
+    if is_case_2_run; H(3)=plot(XO2_FDS_2,GLOB_CHI_R_2,line_fmt{2},'LineWidth',2); end
+    if is_case_3_run; H(4)=plot(XO2_FDS_3,GLOB_CHI_R_3,line_fmt{3},'LineWidth',2); end
+    axis([0.10 0.21 0 0.35 ])
+    xlabel('O2 (vol frac)','Interpreter',Font_Interpreter,'FontSize',Label_Font_Size)
+    ylabel('\chi_R','Interpreter',Font_Interpreter,'FontSize',Label_Font_Size)
+    text(0.105,0.325,[Fuel_name{i_fuel},' Radiative Fraction'],'FontName',Font_Name,'FontSize',Title_Font_Size)
+    set(gca,'FontName',Font_Name)
+    set(gca,'FontSize',Label_Font_Size)
+    lh=legend(H,'Exp',key_fmt{:},'Location','SouthEast');
+    set(lh,'FontName',Font_Name,'FontSize',Key_Font_Size)
+    git_file=[outdir,fuel_name{i_fuel},git_tag_ext];
+    addverstr(gca,git_file,'linear');
+
+    set(gcf,'Visible',Figure_Visibility);
+    set(gcf,'Units',Paper_Units);
+    set(gcf,'PaperUnits',Paper_Units);
+    set(gcf,'PaperSize',[Paper_Width Paper_Height]);
+    set(gcf,'Position',[0 0 Paper_Width Paper_Height]);
+    print(gcf,'-dpdf',[pltdir,fuel_name{i_fuel},'_global_Chi_R']);
+end
+
+
+
+%---------------------- Metric Calculations-------------------------------%
+%-------------------------------------------------------------------------%
+%piecewise linear fit of experimental data
+% for i=1:62
+% GLOB_CHI_EXP(i,1)=interp1(XO2,Chi_R,XO2_FDS_1(i));
+% end
+GLOB_CHI_EXP2=interp1(pw_ramp_XO2,pw_ramp_chir,XO2); % Experimental Fit points with Experimental XO2
+GLOB_CHI_EXP=interp1(pw_ramp_XO2,pw_ramp_chir,XO2_FDS_1); % Experimenta Fit points with Simulation XO2
+GLOB_CHI_mean=(GLOB_CHI_R_3+GLOB_CHI_R_1+GLOB_CHI_R_2)/3; % Mean curve of simulation
+
+Div=GLOB_CHI_EXP2-Chi_R; % Delta of experiment and experiment fit
+Div=Div(~isnan(Div));
+Experimental_RMS=rms(Div); % Experimental RMSU
+
+Diff1=(GLOB_CHI_R_1-GLOB_CHI_mean); % Delta of each grid resolution form mean curve
+Diff2=(GLOB_CHI_R_2-GLOB_CHI_mean);
+Diff3=(GLOB_CHI_R_3-GLOB_CHI_mean);
+Diff_full=[Diff1;Diff2;Diff3]; % Sorting them in a single vector
+Full_range_RMS=rms(Diff_full); % Full range RMSU
+
+Diff=[(GLOB_CHI_R_1(1)-GLOB_CHI_mean(1)),(GLOB_CHI_R_2(1)-GLOB_CHI_mean(1)),(GLOB_CHI_R_3(1)-GLOB_CHI_mean(1))];
+% Delta of each grid resolution from mean curve at 21 %
+Pick_O2_RMS=rms(Diff); % 21%O2 RMSU
+
+Full_range_bias=mean(abs(GLOB_CHI_mean-GLOB_CHI_EXP)); 
+% Mean of absolute delta of mean curve and experimental fit
+
+Pick_O2_bias=abs(GLOB_CHI_mean(1)-GLOB_CHI_EXP(1));
+% Absolute delta of mean curve and experimental fit at 21%O2
+
+Experimental_RMS
+Full_range_RMS
+Pick_O2_RMS
+Full_range_bias
+Pick_O2_bias
+% Figure that explains the RMSU and bias
+figure(6)
+clear H
+H(1)=plot(XO2,Chi_R,'ko')
+hold on
+H(2)=plot(pw_ramp_XO2,pw_ramp_chir,LineWidth=2);
+H(3)=plot(XO2_FDS_1,GLOB_CHI_R_1,'b:','LineWidth',2);
+H(4)=plot(XO2_FDS_2,GLOB_CHI_R_2,'r:','LineWidth',2); 
+H(5)=plot(XO2_FDS_3,GLOB_CHI_R_3,'k:','LineWidth',2);
+H(6)=plot(XO2_FDS_1,GLOB_CHI_mean,LineWidth=2);
+%line_fmt{1}
+axis([0.10 0.21 0 0.35 ]);
+    xlabel('O2 (vol frac)','Interpreter',Font_Interpreter,'FontSize',Label_Font_Size)
+    ylabel('\chi_R','Interpreter',Font_Interpreter,'FontSize',Label_Font_Size)
+    text(0.105,0.325,[Fuel_name{i_fuel},' Radiative Fraction'],'FontName',Font_Name,'FontSize',Title_Font_Size)
+    set(gca,'FontName',Font_Name);
+    set(gca,'FontSize',Label_Font_Size);
+   % legend('EXP','Linear fit',)
+    lh=legend(H,'Exp','Exp. Fit',key_fmt{:},'Mean curve','Location','SouthEast');
+    set(lh,'FontName',Font_Name,'FontSize',Key_Font_Size)
+    git_file=[outdir,fuel_name{i_fuel},git_tag_ext];
+    addverstr(gca,git_file,'linear');
+
+    set(gcf,'Visible',Figure_Visibility);
+    set(gcf,'Units',Paper_Units);
+    set(gcf,'PaperUnits',Paper_Units);
+    set(gcf,'PaperSize',[Paper_Width Paper_Height]);
+    set(gcf,'Position',[0 0 Paper_Width Paper_Height]);
+    print(gcf,'-dpdf',[pltdir,fuel_name{i_fuel},'_Metric']);
